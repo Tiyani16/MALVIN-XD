2b7c043705f45e0015e49cf02a6e08fbf75b7e41
Tiyani16 <ntimbanatiyani@gmail.com>
2025-10-09 00:01:11 +0200

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,8 +1,8 @@
 
 module.exports = {
-  SESSION_ID: "malvin~j9IwDBzK#fDMkI1HCk6qI4jo1--PhYtVk_MrMHJFPNkgtH0hEttUb67d2e0e7cce640d",  // add your Session Id here
+  SESSION_ID: "malvin~WxhXnCDA#rTYrgcuehGH2SGbs-dOoZZ7dkdkrFQC8GQxlZeThYwQ40988ba3584ed89f",  // add your Session Id here
   
-  OWNER_NUMBER: "263714757857", // put your phone number here
+  OWNER_NUMBER: "2635885427", // put your phone number here
   
   PREFIX: ".", // prefix (e.g., ., /, !, *)
   
