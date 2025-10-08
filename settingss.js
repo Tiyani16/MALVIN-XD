f3b7dcd814394b560fa2e42fc6d1595d655065fb
Tiyani16 <ntimbanatiyani@gmail.com>
2025-10-08 14:43:26 +0200

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,6 +1,6 @@
 
 module.exports = {
-  SESSION_ID: "",  // add your Session Id here
+  SESSION_ID: "malvin~j9IwDBzK#fDMkI1HCk6qI4jo1--PhYtVk_MrMHJFPNkgtH0hEttUb67d2e0e7cce640d",  // add your Session Id here
   
   OWNER_NUMBER: "263714757857", // put your phone number here
   
